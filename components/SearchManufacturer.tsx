a5782dc708efcbd0c6856b356609a2ef043d4929
Junaid <junaidhayat33303@gmail.com>
2023-10-22 05:39:48 +0500

    search filter commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,19 @@
 "use client";
+import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types"
 import { Combobox, Transition } from "@headlessui/react"
 import Image from "next/image"
 import { useState, Fragment } from "react";
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState("");
+    const filteredManufacturers = query === ""
+        ? manufacturers
+        : manufacturers.filter((item) =>
+            item
+                .toLowerCase().replace(/\s+/g, "")
+                .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
     return (
         <div className="search-manufacturer">
             <Combobox>
@@ -26,12 +34,25 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                         onChange={(e) => setQuery(e.target.value)}
                     />
                     <Transition as={Fragment}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                    afterLeave={()=>setQuery('')}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        afterLeave={() => setQuery('')}
                     >
-
+                        <Combobox.Options>
+                            {filteredManufacturers.map((item) => (
+                                <Combobox.Option
+                                    key={item}
+                                    className={(({ active }) => `relative search-manufacturer__option 
+                                                    ${active ? "bg-primary-blue text-white"
+                                            : "text-gray-900"}`
+                                    )}
+                                    value={item}
+                                >
+                                    {item}
+                                </Combobox.Option>
+                            ))}
+                        </Combobox.Options>
                     </Transition>
                 </div>
             </Combobox>
