b02b25d1fb8b82e55b2e0a5095e24537a8973182
Junaid <junaidhayat33303@gmail.com>
2023-10-22 23:49:06 +0500

    car type define commit

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+import { CarProps } from '@/types';
+import { useState } from 'react'
 
-const CarCard = () => {
-  return (
-    <div>CarCard</div>
-  )
+interface CarCardProps {
+    car: CarProps
+}
+const CarCard = ({ car }: CarCardProps) => {
+    return (
+        <div>CarCard</div>
+    )
 }
 
 export default CarCard
