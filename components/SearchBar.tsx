6a3a6c8d94b465c2477784f3fe24078242899d2d
Junaid <junaidhayat33303@gmail.com>
2023-10-29 18:20:43 +0500

    search bar commits

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { SearchManufacturer } from "./";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -16,11 +17,43 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   </button>
 )
 const SearchBar = () => {
+
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
-  const handleSearch = () => { }
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === '' && model === '') {
+      alert('please fill the searchbar')
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  }
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set('model', model)
+    } else {
+      searchParams.delete('model')
+    }
+
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer)
+    } else {
+      searchParams.delete('manufacturer')
+    }
+
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPathName);
+  }
+
   return (
     <form className="searchbar" onSubmit={handleSearch}>
+
       <div className="searchbar__item">
         <SearchManufacturer
           manufacturer={manufacturer}
@@ -28,6 +61,7 @@ const SearchBar = () => {
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
+
       <div className="searchbar__item">
         <Image
           src="/model-icon.png"
