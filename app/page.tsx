17698f7493f47278c08f447b58068df1307c51eb
Junaid <junaidhayat33303@gmail.com>
2023-10-31 06:54:28 +0500

    home page commits

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 
@@ -42,7 +42,10 @@ export default async function Home({ searchParams }) {
                   <CarCard car={car} />
                 ))}
               </div>
-              <ShowMore />
+              <ShowMore
+                pageNumber={(searchParams.pageNumber || 10) / 10}
+                isNext={(searchParams.limit || 10) > allCars.length}
+              />
             </section>
           ) : (
             <div className="home__error-container">
