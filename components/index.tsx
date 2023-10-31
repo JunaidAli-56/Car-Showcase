83cea88fefad51d5c31e66388c15a3d4789e8a18
Junaid <junaidhayat33303@gmail.com>
2023-10-31 06:51:14 +0500

    components commit

--- components/index.tsx
+++ components/index.tsx
@@ -2,6 +2,7 @@ import Hero from './Hero';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import CarCard from './CarCard';
+import ShowMore from './ShowMore';
 import SearchBar from './SearchBar';
 import CustomButton from './CustomButton';
 import CustomFilter from './CustomFilter';
@@ -13,6 +14,7 @@ export {
     Navbar,
     Footer,
     CarCard,
+    ShowMore,
     SearchBar,
     CardDetails,
     CustomFilter,
