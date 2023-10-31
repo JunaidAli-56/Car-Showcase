ab76fd151e7fca8effff29b6a486bd11e694a087
Junaid <junaidhayat33303@gmail.com>
2023-11-01 04:12:36 +0500

    show more components commit

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { ShowMoreProps } from '@/types'
 import { useRouter } from 'next/navigation'
+import { ShowMoreProps } from '@/types'
 import { CustomButton } from '@/components';
 import { updateSearchParams } from '@/utils';
 
@@ -9,6 +9,7 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
     const handleNavigation = () => {
         const newLimit = (pageNumber + 1) * 10;
         const newPathName = updateSearchParams("limit", `${newLimit}`);
+        router.push(newPathName);
     }
     return (
         <div className='w-full flex-center gap-5 mt-10'>
