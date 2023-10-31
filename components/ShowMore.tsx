57a66432906585816cb2b9ead0a39cc6e1585e20
Junaid <junaidhayat33303@gmail.com>
2023-10-31 07:06:18 +0500

    show more btn commits

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -2,10 +2,14 @@
 import { ShowMoreProps } from '@/types'
 import { useRouter } from 'next/navigation'
 import { CustomButton } from '@/components';
+import { updateSearchParams } from '@/utils';
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
     const router = useRouter();
-    const handleNavigation = () => { }
+    const handleNavigation = () => {
+        const newLimit = (pageNumber + 1) * 10;
+        const newPathName = updateSearchParams("limit", `${newLimit}`);
+    }
     return (
         <div className='w-full flex-center gap-5 mt-10'>
             {!isNext && (
